9db7918e00afb36585b41fb9b3b66393ef5a01b2
David Angel <david@davidangel.net>
2012-08-11 01:02:14 -0400

    First Commit
    Just trying to set up my most basic starting structure for a WordPress
    theme.

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php
+			/*
+			 * Print the <title> tag based on what is being viewed.
+			 */
+			global $page, $paged;
+		
+			wp_title( '|', true, 'right' );
+		
+			// Add the blog name.
+			bloginfo( 'name' );
+		
+			// Add the blog description for the home/front page.
+			$site_description = get_bloginfo( 'description', 'display' );
+			if ( $site_description && ( is_home() || is_front_page() ) )
+				echo " | $site_description";
+		
+			// Add a page number if necessary:
+			if ( $paged >= 2 || $page >= 2 )
+				echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
+		
+			?></title>
+		<link rel=StyleSheet href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+	</head>
+	<body>
+		<div id="container">
+
